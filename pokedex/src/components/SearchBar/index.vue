e763758e41327b5e59a8103d8f1110a178f8f528
DevaiRJunior12 <contato.devairjunior@gmail.com>
2024-10-03 12:53:36 -0300

    <Create> Barra de Pesquisa

---
+++ pokedex/src/components/SearchBar/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <input class="pokedex-input" type="text" v-model="search" @input="onSearch" placeholder="Buscar por nome ou número..." />
+</template>
+
+<script lang="ts">
+import './styles.css';
+
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  emits: ['search'],
+  setup(_, { emit }) {
+    const search = ref('');
+    const onSearch = () => {
+      emit('search', search.value);
+    };
+    return { search, onSearch };
+  }
+});
+</script>
